ed029e581e57811113e8959aa25e71c7509ab35a
Milkenm <miguel.campos.johnny@gmail.com>
2023-01-12 17:00:44 +0000

    Tweak bump delay a lil bit more

--- scripts/content.js
+++ scripts/content.js
@@ -30,6 +30,6 @@ function bumpAllTrades()
     for (let i = 0; i < bumpButtons.length; i++) {
         setTimeout(() => {
             bumpButtons[i].click();
-        }, i * 1100);
+        }, i * 1250);
     }
 }
