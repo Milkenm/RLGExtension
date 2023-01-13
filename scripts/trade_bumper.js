4a2e5199f3a9abe78802b02a1477f1ff595e3916
Milkenm <miguel.campos.johnny@gmail.com>
2023-01-13 11:22:53 +0000

    Change "Bump All" delay from 1.25s to 1.3s and change manifest stuff

--- scripts/trade_bumper.js
+++ scripts/trade_bumper.js
@@ -1,29 +1,14 @@
-﻿// Check if the user is at the trades page
-if (window.location.href.startsWith("https://rocket-league.com/trades/")) {
-    // Get the "select all trades" checkbox
-    const referenceCheckbox = document.getElementById("rlg-trade-select-all");
+﻿// Get the "select all trades" checkbox
+const referenceCheckbox = document.getElementById("rlg-trade-select-all");
 
-    // Create the "Bump All" button
-    let bumpAllButton = document.createElement("button");
-    bumpAllButton.textContent = "Bump All";
-    bumpAllButton.id = "rlge-bump-all-trades-btn";
-    bumpAllButton.classList = "rlge-btn-bump-all-trades rlg-btn-primary";
-    bumpAllButton.addEventListener("click", bumpAllTrades);
-
-    // Check if the checkbox was found
-    if (referenceCheckbox)
-    {
-        // Get the checkbox's parent div
-        let div = referenceCheckbox.parentElement;
-
-        // Add the "Bump All" button to the page
-        div.insertBefore(bumpAllButton, referenceCheckbox.nextSibling);
-    }
-}
-
-function bumpAllTrades()
-{
-    const bumpDelay = 1250;
+// Create the "Bump All" button
+let bumpAllButton = document.createElement("button");
+bumpAllButton.textContent = "Bump All";
+bumpAllButton.id = "rlge-bump-all-trades-btn";
+bumpAllButton.classList = "rlge-btn-bump-all-trades rlg-btn-primary";
+// "Bump All" button click
+bumpAllButton.addEventListener("click", () => {
+    const bumpDelay = 1300;
 
     // Get all "Bump" buttons
     let bumpButtons = document.getElementsByClassName("rlg-trade__bump");
@@ -31,7 +16,7 @@ function bumpAllTrades()
     const successPopup = document.getElementsByClassName("rlg-site-popup")[0];
     successPopup.style.display = "none";
 
-    // Click all "Bump" buttons with a 2.5s delay inbetween
+    // Click all "Bump" buttons with a 1.3s delay inbetween
     for (let i = 0; i < bumpButtons.length; i++) {
         setTimeout(() => {
             bumpButtons[i].click();
@@ -42,4 +27,13 @@ function bumpAllTrades()
         successPopup.style.display = null;
         alert("Bumped all trades.");
     }, bumpButtons.length - 1 * bumpDelay);
+});
+
+// Check if the checkbox was found
+if (referenceCheckbox) {
+    // Get the checkbox's parent div
+    let div = referenceCheckbox.parentElement;
+
+    // Add the "Bump All" button to the page
+    div.insertBefore(bumpAllButton, referenceCheckbox.nextSibling);
 }
